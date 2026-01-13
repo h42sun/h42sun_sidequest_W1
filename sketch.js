3a9873ec549266e809d4efc3560f50db93c7ed64
Haojia Sun <h42sun@uwaterloo.ca>
2026-01-12 21:16:49 -0500

    Added text inspired by Jenga in middle of the canvas.

--- sketch.js
+++ sketch.js
@@ -7,4 +7,11 @@ function draw() {
   fill(255, 255, 0);
   noStroke();
   rect(0, 0, 200, 400);
+
+  fill(255, 255, 255);
+  stroke(0, 0, 0);
+  strokeWeight(6);
+  textAlign(CENTER, CENTER);
+  textSize(30);
+  text("Patience is key - \nDon't lose your balance!", 200, 200);
 }
